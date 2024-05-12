38ddb363c76b90b53ff776813974f2a97df1c1e7
Rahul <rituq@LAPTOP-V361DO72>
2024-05-12 17:09:21 +0530

    Added getAllProducts

--- ProductService/src/main/java/com/productservice/controller/ProductController.java
+++ ProductService/src/main/java/com/productservice/controller/ProductController.java
@@ -1,5 +1,13 @@
 package com.productservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
+	@GetMapping("/")
+	public String getAllProducts() {
+		return "Getting All Products";
+	}
 
 }
